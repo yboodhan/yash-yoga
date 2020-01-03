264960e71a4fd0dba82eb1cd17503f2630d4866c
yboodhan <yboodhan@gmail.com>
2020-01-03 15:54:57 -0800

    Added repos table

--- controllers/profile.js
+++ controllers/profile.js
@@ -15,28 +15,30 @@ router.get('/admin', isAdminLoggedIn, (req, res) => {
 
 //GET profile/repos
 router.get('/repos', isLoggedIn, (req, res) => {
-    if (req.user.githubToken) {
-        //Grab page is exists
-        let page = parseInt(req.query.page) || 1
 
-        //Make sure user has github
-        axios.get('https://api.github.com/user/repos?per_page=' + page, {
-            headers: {
-                Authorization: `token ${req.user.githubToken}`,
-                'User-Agent': 'OAuth-GH-App'
-            }
-        })
-        .then(response => {
-            console.log(response.data)
-            res.render('profile/repos', { repos: response.data, page })
-        })
-        .catch(err=>{
-            console.log(err)
-            res.render('error')
-        })
-    } else {
-        res.render('profile/repos', { repos: response.data, page })
-    }
+    // Grab page number if it exists
+    let page = parseInt(req.query.page) || 1
+
+  // Make sure user has GH info
+  if (req.user.githubToken) {
+    axios.get('https://api.github.com/user/repos?per_page=10&page=' + page, {
+      headers: {
+        Authorization: `token ${req.user.githubToken}`,
+        'User-Agent': 'Yashoma-GH-App'
+      }
+    })
+    .then(response => {
+      res.render('profile/repos', { repos: response.data, page })
+    })
+    .catch(err => {
+      console.log(err)
+      res.render('error')
+    })
+  }
+  else {
+    // No Github Info
+    res.render('profile/repos', { repos: null, page })
+  }
 })
 
 // Export the router object so we can include it in the other files
