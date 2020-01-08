4d223ad2920993f4d0e8c510b81175dd819cbfad
yboodhan <yboodhan@gmail.com>
2020-01-07 16:27:20 -0800

    Changed routines_poses model

---
+++ migrations/20200108000200-create-routines-poses.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('routines_poses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      routineId: {
+        type: Sequelize.INTEGER
+      },
+      poseId: {
+        type: Sequelize.INTEGER
+      },
+      duration: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('routines_poses');
+  }
+};
--- models/routines_poses.js
+++ models/routines_poses.js
@@ -2,7 +2,8 @@
 module.exports = (sequelize, DataTypes) => {
   const routines_poses = sequelize.define('routines_poses', {
     routineId: DataTypes.INTEGER,
-    poseId: DataTypes.INTEGER
+    poseId: DataTypes.INTEGER,
+    duration: DataTypes.INTEGER
   }, {});
   routines_poses.associate = function(models) {
     // associations can be defined here
