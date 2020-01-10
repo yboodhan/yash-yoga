ddd2da6cfc2706511647bc2b7c2074d6205c9258
yboodhan <yboodhan@gmail.com>
2020-01-09 21:41:15 -0800

    Completed edit update route for user

--- controllers/profile.js
+++ controllers/profile.js
@@ -2,6 +2,7 @@
 let router = require('express').Router()
 let isAdminLoggedIn = require('../middleware/isAdminLoggedIn')
 let isLoggedIn = require('../middleware/isLoggedIn')
+let db = require('../models')
 
 // Define routes
 router.get('/', isLoggedIn, (req, res) => {
@@ -14,6 +15,26 @@ router.get('/admin', isAdminLoggedIn, (req, res) => {
 
 router.get('/edit', isLoggedIn, (req, res) => {
   res.render('profile/edit')
+})
+
+router.put('/', isLoggedIn, (req, res) => {
+  db.user.update({
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    email: req.body.email,
+    username: req.body.username,
+    photoUrl: req.body.photoUrl,
+    bio: req.body.bio
+  },{
+    where: { id: req.user.id }
+  })
+  .then( routine => {
+    res.redirect('/profile')
+  })
+  .catch( (error) => {
+    console.log(error)
+    res.render('error')
+})
 })
 
 // Export the router object so we can include it in the other files
