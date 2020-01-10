8eab6f259714f657158199256431f99d2217050a
yboodhan <yboodhan@gmail.com>
2020-01-09 23:20:41 -0800

    Added show for each journal entry

--- controllers/inspiration.js
+++ controllers/inspiration.js
@@ -1,7 +1,6 @@
 let router = require('express').Router()
 let db = require('../models')
 let axios = require('axios')
-let moment = require('moment')
 let isLoggedIn = require('../middleware/isLoggedIn')
 
 router.get('/', isLoggedIn, (req, res) => {
--- controllers/journal.js
+++ controllers/journal.js
@@ -1,8 +1,18 @@
 let router = require('express').Router()
 let isLoggedIn = require('../middleware/isLoggedIn')
+let db = require('../models')
 
 router.get('/', isLoggedIn, (req, res) => {
-    res.render('user/journal/index')
+    db.entry.findAll({
+        where: {userId: req.user.id}
+    })
+    .then( entries => {
+        res.render('user/journal/index', { entries })
+    })
+    .catch( (error) => {
+        console.log(error)
+        res.render('error')
+    })
 })
 
 router.get('/new', isLoggedIn, (req, res) => {
@@ -10,7 +20,31 @@ router.get('/new', isLoggedIn, (req, res) => {
 })
 
 router.post('/', isLoggedIn, (req, res) => {
-    res.send(req.body)
+    db.entry.create({
+        title: req.body.title,
+        content: req.body.content,
+        userId: req.user.id
+    })
+    .then( (entry) => {
+        res.redirect('/journal')
+    })
+    .catch( (error) => {
+        console.log(error)
+        res.render('error')
+    })
+})
+
+router.get('/:id', isLoggedIn, (req, res) => {
+    db.entry.findOne({
+        where: { id: req.params.id }
+    })
+    .then( entry => {
+        res.render('user/journal/show', { entry })
+    })
+    .catch( (error) => {
+        console.log(error)
+        res.render('error')
+    })
 })
 
 module.exports = router
