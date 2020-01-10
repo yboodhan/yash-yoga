3306a0a196b83280c0105de2b8c71d0cd215b58a
yboodhan <yboodhan@gmail.com>
2020-01-09 21:47:14 -0800

    Removed articles from app

--- controllers/articles.js
+++
@@ -1,8 +0,0 @@
-let router = require('express').Router()
-let isLoggedIn = require('../middleware/isLoggedIn')
-
-router.get('/', isLoggedIn, (req, res) => {
-    res.render('user/articles/index')
-})
-
-module.exports = router
